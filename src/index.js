9dc76fe506f4c7f5687ebe48cf96c64d40f88962
GereyevKamran <jsfront20@gmail.com>
2020-12-12 22:37:43 +0400

    edit

--- src/index.js
+++ src/index.js
@@ -1,31 +1,11 @@
 import "./style.less";
-<<<<<<< HEAD
-import View from "./view";
-import Controller from "./Controller";
-import Model from "./Model";
-
-function init() {
-  const view = new View();
-  // const controller = new Controller();
-  // const model = new Model();
-
-  // controller.init();
-  // model.init();
-  view.init();
-  view.addColumnbButton.addEventListener("click", view.createListContainer);
-}
-
-init();
-=======
 import Controller from "./Controller";
 
 function start() {
-      //const view = new View();
-      //const model = new Model();
-      const controller = new Controller();
-      console.log(controller);
-  }
-  
+  //const view = new View();
+  //const model = new Model();
+  const controller = new Controller();
+  console.log(controller);
+}
+
 start();
- 
->>>>>>> mvc
